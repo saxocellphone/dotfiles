4286d1295137a74ec3b1813522f423757220820b
Victor Nazzaro <nazzav923@gmail.com>
2021-08-03 14:09:11 -0400

    fix path dup

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,3 @@
 # ghcup-env
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
-set -U fish_user_path ~/.local/bin $fish_user_path
 test -f /home/vnazzaro/.ghcup/env ; and set -gx PATH $HOME/.cabal/bin /home/vnazzaro/.ghcup/bin $PATH
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
