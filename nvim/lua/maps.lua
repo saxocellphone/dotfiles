035efe1658566dc764e81963f6b20d2b711a213d
Victor Nazzaro <nazzav923@gmail.com>
2021-08-23 09:54:14 -0400

    add good mappings

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -9,8 +9,9 @@ vim.g.mapleader = ';'  -- 'vim.g' sets global variables
 --local noremap = { noremap = true }
 local silent = { silent = true }
 utils.map('n', '<leader>n', ':nohlsearch<cr>')
-utils.map('n', '<leader>k', ':bnext<cr>')
-utils.map('n', '<leader>j', ':bprev<cr>')
+utils.map('n', 'Y', 'y$')
+utils.map('n', 'n', 'nzzzv')
+utils.map('n', 'N', 'Nzzzv')
 
 utils.map('n', '<C-j>', '<c-w>j')
 utils.map('n', '<C-k>', '<c-w>k')
@@ -18,7 +19,7 @@ utils.map('n', '<C-l>', '<c-w>l')
 utils.map('n', '<C-h>', '<c-w>h')
 
 utils.map('n', '<cr>', 'o<esc>', silent)
-utils.map('n', 'zz', ':w|bd<cr>', silent)
+utils.map('n', '``', ':w|bd<cr>', silent)
 
 utils.map('n', '<C-s>', ':w<cr>')
 
@@ -29,9 +30,22 @@ utils.map('v', '<space>', 'zf')
 
 -- Insert mode stuff
 utils.map('i', 'wj', '<Esc>')
+utils.map('i', ',', ',<c-g>u')
+utils.map('i', '.', '.<c-g>u')
+utils.map('i', '!', '!<c-g>u')
+utils.map('i', '?', '?<c-g>u')
+utils.map('i', '[', '[<c-g>u')
 
 -- Indent
 utils.map('n', '<', '<<')
 utils.map('n', '>', '>>')
 utils.map('v', '<', '<gv')
 utils.map('v', '>', '>gv')
+
+-- Moving code
+utils.map('n', '<leader>j', ':m .+1<cr>==')
+utils.map('n', '<leader>k', ':m .-2<cr>==')
+utils.map('i', '<c-j>', '<esc>:m .+1<cr>==i')
+utils.map('i', '<c-k>', '<esc>:m .-2<cr>==i')
+utils.map('v', 'J', ":m '>+1<cr>gv=gv")
+utils.map('v', 'K', ":m '<-2<cr>gv=gv")
