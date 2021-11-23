480c3699503fc3813a4192ff7172586cc834df5e
Victor Nazzaro <vnazzaro@cisco.com>
2021-11-22 21:30:07 -0500

    remove paq and user packer

--- nvim/lua/plugs.lua
+++ nvim/lua/plugs.lua
@@ -1,60 +1,61 @@
-vim.cmd 'packadd paq-nvim'
-require "paq" {
-    "savq/paq-nvim";                  -- Let Paq manage itself
+vim.cmd 'packadd packer.nvim'
+return require("packer").startup(function()
+    -- Let packer manage itself
+    use 'wbthomason/packer.nvim'
 
     -- Git
-    "lewis6991/gitsigns.nvim";
+    use "lewis6991/gitsigns.nvim"
 
     -- File/Grep finder stuff
-    "nvim-telescope/telescope.nvim";
-    "nvim-telescope/telescope-frecency.nvim";
-    "nvim-telescope/telescope-smart-history.nvim";
-    "tami5/sql.nvim";
-    "nvim-lua/popup.nvim";
-    "nvim-lua/plenary.nvim";
-    {"ray-x/guihua.lua", run="cd lua/fzy && make"};
-    {"junegunn/fzf", run="fzf#install()"};
-    "junegunn/fzf.vim";
+    use "nvim-telescope/telescope.nvim"
+    use "nvim-telescope/telescope-frecency.nvim"
+    use "nvim-telescope/telescope-smart-history.nvim"
+    use "tami5/sqlite.lua"
+    use "nvim-lua/popup.nvim"
+    use "nvim-lua/plenary.nvim"
+    use {"ray-x/guihua.lua", run="cd lua/fzy && make"}
+    use {"junegunn/fzf", run="fzf#install()"}
+    use "junegunn/fzf.vim"
 
     -- Appearance
-    "joshdick/onedark.vim";
-    "romgrk/barbar.nvim";
-    "yamatsum/nvim-cursorline";
-    "windwp/windline.nvim";
+    use "joshdick/onedark.vim"
+    use "romgrk/barbar.nvim"
+    use "yamatsum/nvim-cursorline"
+    use "windwp/windline.nvim"
 
     -- Utils
-    "akinsho/nvim-toggleterm.lua";
-    "kyazdani42/nvim-tree.lua";
-    "kyazdani42/nvim-web-devicons";
-    "scrooloose/nerdcommenter";
-    "qpkorr/vim-bufkill";
-    "rafcamlet/nvim-luapad";
-    {"prettier/vim-prettier", run="npm install"};
+    use "akinsho/nvim-toggleterm.lua"
+    use "kyazdani42/nvim-tree.lua"
+    use "kyazdani42/nvim-web-devicons"
+    use "scrooloose/nerdcommenter"
+    use "qpkorr/vim-bufkill"
+    use "rafcamlet/nvim-luapad"
+    use {"prettier/vim-prettier", run="npm install"}
 
     -- Testing
-    "vim-test/vim-test";
-    "google/vim-maktaba";
-    "google/vim-coverage";
-    {"michaelb/sniprun", run="bass bash install.sh"};  --Code runner
+    use "vim-test/vim-test"
+    use "google/vim-maktaba"
+    use "google/vim-coverage"
+    use {"michaelb/sniprun", run="bass bash install.sh"}
 
     -- TreeSitter
-		{"nvim-treesitter/nvim-treesitter", run=":TSUpdate", branch="0.5-compat"};
-    "nvim-treesitter/playground";
+		use {"nvim-treesitter/nvim-treesitter", run=":TSUpdate", branch="0.5-compat"}
+    use "nvim-treesitter/playground"
 
     -- LSP
-    "glepnir/lspsaga.nvim";
-    "neovim/nvim-lspconfig";
-    "hrsh7th/nvim-compe";
-    "glepnir/lspsaga.nvim";
-    "ray-x/lsp_signature.nvim";
+    use "glepnir/lspsaga.nvim"
+    use "neovim/nvim-lspconfig"
+    use "hrsh7th/nvim-compe"
+    use "glepnir/lspsaga.nvim"
+    use "ray-x/lsp_signature.nvim"
 
     -- Snippets
-    "rafamadriz/friendly-snippets";
-    "hrsh7th/vim-vsnip";
-    "hrsh7th/vim-vsnip-integ";
+    use "rafamadriz/friendly-snippets"
+    use "hrsh7th/vim-vsnip"
+    use "hrsh7th/vim-vsnip-integ"
 
     -- Navigator for tmux
-		"christoomey/vim-tmux-navigator"
+		use "christoomey/vim-tmux-navigator"
 
     -- Debugging
     --"puremourning/vimspector";
@@ -77,4 +78,7 @@ require "paq" {
     --{"neoclide/coc.nvim", branch= "release"};
     --"honza/vim-snippets";
     --"sheerun/vim-polyglot";
-}
+		if packer_bootstrap then
+			require('packer').sync()
+		end
+end)
