03fe7f9989b56ca02585651b21d41b7fb897be8e
Victor Nazzaro <nazzav923@gmail.com>
2022-01-26 12:12:01 -0500

    changed bg wallpaper

--- i3/scripts/wallpaper.sh
+++ i3/scripts/wallpaper.sh
@@ -1,2 +1,3 @@
 #feh --bg-scale ~/Documents/images/wallpapers/copycat.png
-feh --bg-scale ~/Documents/images/wallpapers/windmill.jpg
+#feh --bg-scale ~/Documents/images/wallpapers/windmill.jpg
+feh --bg-scale ~/Documents/images/wallpapers/city.jpg
